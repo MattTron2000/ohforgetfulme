ea81b2b3e6a8b3b68d9aa9ac2663a887dcf15188
Matthew Hamilton <matthamilton23@gmail.com>
2013-05-23 20:41:39 -0600

    working prototype

--- app/models/remind.rb
+++ app/models/remind.rb
@@ -1,13 +1,7 @@
 
 class Remind < ActiveRecord::Base
-    def findreminderstodo(reminds)
-        reminds.each do |name|
-            if date.now > reminds.date
-                if time.now > reminds.time
-            
-        end
-
-    end
-
+    # def find_reminders_todo
+    #     @remind = remind
+        
         
 end
