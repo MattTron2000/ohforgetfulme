b3bab7527efdf3f758349490b5a6bc7d9ae248db
Matthew Hamilton <matthamilton23@gmail.com>
2013-05-24 22:21:45 -0600

    Error popup looks pretty, and form validates

--- app/models/remind.rb
+++ app/models/remind.rb
@@ -1,11 +1,19 @@
 
 class Remind < ActiveRecord::Base
+    validates :contactinfo, :presence => true
+    validates :name , :presence => true
+    validates :date, :presence => true
+
     def self.find_reminders_todo
         sent = 0
         Remind.all.each do |x|
-            if Time.now > x.date
-                # ReminderMailer.reminder_email(x).deliver
-                sent += 1
+            if Time.now >= x.date
+                if x.done == nil
+                    x.done = true
+                    x.save 
+                    ReminderMailer.reminder_email(x).deliver
+                    sent += 1
+                end
             end
         end  
         return sent
